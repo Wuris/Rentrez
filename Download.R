7abebae5493302970c7d61c524d8849ca32414b5
Wuris <wucy18cw8@gmail.com>
2022-02-16 16:39:45 -0500

    Part I for Assignment 5
    Create the R file called Download.R, which output the csv file called Sequences.csv

---
+++ Download.R
@@ -0,0 +1,16 @@
+ncbi_ids <- c("HQ433692.1","HQ433694.1","HQ433691.1") # List three IDs that we want
+library(rentrez)  # Load the Package
+Bburg <- entrez_fetch(db = "nuccore", id = ncbi_ids, rettype = "fasta") #Download the data from database called "nuccore", with the id we want (input into the vector called ncbi_ids), in format of fasta.
+
+Sequences <- strsplit(Bburg, split = "\n\n", fixed = T)
+
+print(Sequences)
+
+Sequences <- unlist(Sequences)
+
+header <- gsub("(^>.*sequence)\\n[ATCG].*", "\\1", Sequences)
+seq <- gsub("^>.*sequence\\n([ATCG].*)", "\\1", Sequences)
+Sequences <- data.frame(Name = header, Sequence = seq)
+
+Sequences$Sequence <- gsub("\n", "", Sequences$Sequence)
+write.csv(Sequences, "./Sequences.csv", row.names = F)
