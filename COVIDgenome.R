125e59aedc37e921620509ee2d30a322a34da4fd
Wuris <wucy18cw8@gmail.com>
2022-02-16 21:08:04 -0500

    Update COVIDgenome.R

--- COVIDgenome.R
+++ COVIDgenome.R
@@ -1,3 +1,8 @@
+# Project Info
+# **GitHub user name**: Wuris
+# **Date**: 2022/2/16
+# **GitHub Link**: https://github.com/Wuris/Rentrez.git
+
 ncbi_ids <- c("NC_045512.2") # List the ID that we want
 library(rentrez)  # Load the Package
 Sprotein <- entrez_fetch(db = "nuccore", id = ncbi_ids, rettype = "fasta") #Download the data from database called "nuccore", with the id we want in format of fasta.
@@ -23,3 +28,12 @@ Sprotein <- gsub("\\s", "", Spro2)
 
 # Show the sequence we found
 paste("The sequence of Spike glucoprotein is :", Sprotein)
+
+# Would you say this gene is highly conserved or evolving rapidly? Why? Explain this in a comment.
+# I would say this gene is highly conserved. 
+# First, the E-value for all the 100 result we found is 0, and all Query Cover value is 100%.
+# Which we know that E-value  is a number that describes how many times you would expect a match by chance in a database of that size. The lower the E value is, the more significant the match.
+# And the query cover is a number that describes how much of the query sequence is covered by the target sequence. If the target sequence in the database spans the whole query sequence, then the query cover is 100%. 
+# Second, as for the result under tab "Graphic Summary", we have all results shows in red, which means the Alignment Scores are all equal to or greater than 200.
+# Where the alignment score is computed by assigning a value to each aligned pair of letters and then summing these values over the length of the alignment.
+# Thus, I think the Spike glucoprotein is highly conserved.
